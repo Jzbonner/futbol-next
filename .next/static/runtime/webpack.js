1f0eb3422a8d35afa2f28ee37351744ca6fdf9cc
Jzbonner <jarrett.bonner@gmail.com>
2019-03-23 22:14:29 -0400

    changes to futbol-next functionality and setup for new branch

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9ceb33d6a6c39cfb8bf";
+/******/ 	var hotCurrentHash = "4ab63bd07e22e49cfa50";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
